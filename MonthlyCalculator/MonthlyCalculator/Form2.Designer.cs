8390b2165fc9d5949f5ea625d174c00466537a8f
pradeep mishra <pkmishra.pradeep@gmail.com>
2017-03-16 00:58:43 +0530

    added project

---
+++ MonthlyCalculator/MonthlyCalculator/Form2.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MonthlyCalculator
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.flatrent = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // flatrent
+            // 
+            this.flatrent.AutoSize = true;
+            this.flatrent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatrent.ForeColor = System.Drawing.Color.DarkGreen;
+            this.flatrent.Location = new System.Drawing.Point(36, 33);
+            this.flatrent.Name = "flatrent";
+            this.flatrent.Size = new System.Drawing.Size(74, 17);
+            this.flatrent.TabIndex = 0;
+            this.flatrent.Text = "Flat Rent";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(36, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Maintenance";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label2.Location = new System.Drawing.Point(36, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Maid/Cook";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label3.Location = new System.Drawing.Point(36, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Maid/Room Cleaner";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label4.Location = new System.Drawing.Point(36, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "TV Recharge";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label5.Location = new System.Drawing.Point(36, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Electricity ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label6.Location = new System.Drawing.Point(36, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Water ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label7.Location = new System.Drawing.Point(36, 219);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Big Basket";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Maroon;
+            this.textBox1.Location = new System.Drawing.Point(191, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(71, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 426);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flatrent);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MonthlyCalculator";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label flatrent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ MonthlyCalculator/MonthlyCalculator/LoginForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MonthlyCalculator
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.LoginID = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.txtboxlogin = new System.Windows.Forms.TextBox();
+            this.txtboxpwd = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.loginmsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoginID
+            // 
+            this.LoginID.AutoSize = true;
+            this.LoginID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginID.ForeColor = System.Drawing.Color.DarkGreen;
+            this.LoginID.Location = new System.Drawing.Point(98, 104);
+            this.LoginID.Name = "LoginID";
+            this.LoginID.Size = new System.Drawing.Size(68, 17);
+            this.LoginID.TabIndex = 0;
+            this.LoginID.Text = "Login ID";
+            this.LoginID.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.ForeColor = System.Drawing.Color.DarkGreen;
+            this.Password.Location = new System.Drawing.Point(98, 149);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(77, 17);
+            this.Password.TabIndex = 1;
+            this.Password.Text = "Password";
+            this.Password.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtboxlogin
+            // 
+            this.txtboxlogin.Location = new System.Drawing.Point(194, 104);
+            this.txtboxlogin.Name = "txtboxlogin";
+            this.txtboxlogin.Size = new System.Drawing.Size(189, 20);
+            this.txtboxlogin.TabIndex = 3;
+            this.txtboxlogin.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtboxpwd
+            // 
+            this.txtboxpwd.Location = new System.Drawing.Point(194, 149);
+            this.txtboxpwd.Name = "txtboxpwd";
+            this.txtboxpwd.Size = new System.Drawing.Size(189, 20);
+            this.txtboxpwd.TabIndex = 4;
+            this.txtboxpwd.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnlogin.Location = new System.Drawing.Point(194, 198);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(84, 35);
+            this.btnlogin.TabIndex = 5;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // loginmsg
+            // 
+            this.loginmsg.AutoSize = true;
+            this.loginmsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginmsg.ForeColor = System.Drawing.Color.ForestGreen;
+            this.loginmsg.Location = new System.Drawing.Point(191, 66);
+            this.loginmsg.Name = "loginmsg";
+            this.loginmsg.Size = new System.Drawing.Size(0, 17);
+            this.loginmsg.TabIndex = 6;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 284);
+            this.Controls.Add(this.loginmsg);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtboxpwd);
+            this.Controls.Add(this.txtboxlogin);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.LoginID);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MonthlyCalculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LoginID;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox txtboxlogin;
+        private System.Windows.Forms.TextBox txtboxpwd;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Label loginmsg;
+    }
+}
+
